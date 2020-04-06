1d18954cc8a4044f09739e2f092b5f2611f78b8d
AHMED AFANDY <eng.computer1@yahoo.com>
2020-04-06 06:16:11 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -12,9 +12,9 @@ class Doctor
     end
 
       def new_appointment(date:, patient:)
-          @date=date
-          @patient=patient
-          Appointment.new(@date,@patient,self)
+          date=date
+          patient=patient
+          Appointment.new(date,patient,self)
       end
 
       def appointments
